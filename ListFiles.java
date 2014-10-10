37f6f9c044b63433601be6999d2a572093642cf5
dnkamila <dnkamila@gmail.com>
2014-10-10 17:28:25 +0700

    Update ListFiles.java
    Sudah cetak ke file tp belom handle kalau temporary file log sudah ada di komputer

--- ListFiles.java
+++ ListFiles.java
@@ -1,34 +1,50 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class ListFiles {
-	public static void listFilesAndFilesSubDirectories(String sharedFolder,
-			String directoryName) {
+	public static void listFiles(String sharedFolder) {
+		try {
+			//belom handle kalau sudah ada log.txt
+			File file = new File("log.txt");
 
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			listFilesHelper(bw, sharedFolder, sharedFolder);
+			bw.close();
+		}
+		
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void listFilesHelper(BufferedWriter bw, String sharedFolder, String directoryName) throws IOException {
 		File directory = new File(directoryName);
-
-		// get all the files from a directory
 		File[] fList = directory.listFiles();
 
 		for (File file : fList) {
 			if (file.isFile()) {
 				String absolute = file.getAbsolutePath();
-
-				String relative = new File(sharedFolder).toURI()
-						.relativize(new File(absolute).toURI()).getPath();
-
-				System.out.println(relative + ";" + file.lastModified());
-			} else if (file.isDirectory()) {
-				listFilesAndFilesSubDirectories(sharedFolder,
-						file.getAbsolutePath());
+				String relative = new File(sharedFolder).toURI().relativize(new File(absolute).toURI()).getPath();
+				
+				bw.write(relative + ";" + file.lastModified() + "\n");
+			}
+			
+			else if (file.isDirectory()) {
+				listFilesHelper(bw, sharedFolder, file.getAbsolutePath());
 			}
 		}
 	}
 
 	public static void main(String[] args) {
 		String directoryWindows = "D:\\Portable_IDM";
-		System.out.println(directoryWindows);
-		
-		ListFiles.listFilesAndFilesSubDirectories(directoryWindows,
-				directoryWindows);
+		ListFiles.listFiles(directoryWindows);
 	}
 }
