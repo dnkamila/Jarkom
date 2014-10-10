9bda7b1acc61006736985d39f2f07276b235fd9c
dnkamila <dnkamila@gmail.com>
2014-10-10 16:51:01 +0700

    Update and rename ListFilesUtil.java to ListFiles.java

---
+++ ListFiles.java
@@ -0,0 +1,34 @@
+import java.io.File;
+
+public class ListFiles {
+	public static void listFilesAndFilesSubDirectories(String sharedFolder,
+			String directoryName) {
+
+		File directory = new File(directoryName);
+
+		// get all the files from a directory
+		File[] fList = directory.listFiles();
+
+		for (File file : fList) {
+			if (file.isFile()) {
+				String absolute = file.getAbsolutePath();
+
+				String relative = new File(sharedFolder).toURI()
+						.relativize(new File(absolute).toURI()).getPath();
+
+				System.out.println(relative + ";" + file.lastModified());
+			} else if (file.isDirectory()) {
+				listFilesAndFilesSubDirectories(sharedFolder,
+						file.getAbsolutePath());
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		String directoryWindows = "D:\\Portable_IDM";
+		System.out.println(directoryWindows);
+		// listFilesUtil.listFiles(directoryWindows);
+		ListFiles.listFilesAndFilesSubDirectories(directoryWindows,
+				directoryWindows);
+	}
+}
