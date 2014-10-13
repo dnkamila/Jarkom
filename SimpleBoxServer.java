1c2e82d398ef37c93aea1beb2de0b85e3f828280
syntaxerror007 <oenangmartin@gmail.com>
2014-10-13 20:06:48 +0700

    Update SimpleBoxServer.java

--- SimpleBoxServer.java
+++ SimpleBoxServer.java
@@ -1,17 +1,19 @@
-import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.DataInputStream;
-import java.io.FileOutputStream;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SimpleBoxServer {
 	private static ServerSocket serverSocket = null;
 	private static Socket clientSocket = null;
-	private static final int PORT_NUMBER = 7718;
+	private static final int PORT_NUMBER = 7736;
 	private static final ArrayList<ThreadClient> listThreads = new ArrayList<ThreadClient>();
 
 	public static void main(String[] args) {
@@ -42,102 +44,114 @@ class ThreadClient extends Thread {
 	private String clientName;
 	private String sharedFolderPath;
 	private Socket clientSocket;
-	private DataInputStream dis;
-	private InputStream is;
+	private DataInputStream is;
 	private PrintStream os;
-	private FileOutputStream fos;
-	private BufferedOutputStream bos;
 
 	public ThreadClient(Socket clientSocket, ArrayList<ThreadClient> listThreads) {
 		this.listThreads = listThreads;
 		this.clientSocket = clientSocket;
 	}
 
+	public void RequestFile(ArrayList<String> listOfFiles) {
+
+		try {
+			InputStream ris = clientSocket.getInputStream();
+			for (int i = 0; i < listOfFiles.size(); i++) {
+				os.println("request;" + listOfFiles.get(i));
+				byte[] byteReceived = new byte[10000000];
+				ris.read(byteReceived, 0, byteReceived.length);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	@SuppressWarnings("deprecation")
 	public void run() {
 		ArrayList<ThreadClient> listThreads = this.listThreads;
 		try {
-//			dis = new DataInputStream(clientSocket.getInputStream());
-			is = clientSocket.getInputStream();
+
+			is = new DataInputStream(clientSocket.getInputStream());
 			os = new PrintStream(clientSocket.getOutputStream());
 
-			String path;
-//			while (true) {
-//				os.println("Please enter your shared folder path.");
-//				path = dis.readLine().trim();
-//				if (path != null && !path.equals("")) {
-//					break;
-//				} else {
-//					os.println("You should type your shared folder path");
-//				}
-//			}
+			while (true) {
+				String line = is.readLine();
+				ListFiles.listFiles(".", "server");
+				File logFile = new File("log_server.txt");
+				BufferedReader readFile;
+				readFile = new BufferedReader(new FileReader(logFile));
+				String filesOnServer = readFile.readLine();
+				ArrayList<String> listFilesOnServer = new ArrayList<String>(
+						Arrays.asList(filesOnServer.split(";")));
+				ArrayList<String> fileNotFoundOnClient = new ArrayList<String>();
+				ArrayList<String> fileNotFoundOnServer = new ArrayList<String>();
+				String requestToClient = "";
+				String requestToServer = "";
+				readFile.close();
+				if (line.indexOf("check") != -1) {
+					for (int i = 0; i < listThreads.size(); i++) {
+						if (listThreads.get(i) != null) {
+							if (listThreads.get(i) == this) {
+								ArrayList<String> listOfFiles = new ArrayList<String>(
+										Arrays.asList(line.split(";")));
+								for (int j = 1; j < listOfFiles.size(); j++) {
+									// jika file pada client tidak terdapat pada
+									// server
+									if (!listFilesOnServer.contains(listOfFiles
+											.get(j))) {
+										// ini nanti diadd
+										fileNotFoundOnClient.add(listOfFiles
+												.get(j).split("!")[0]);
+										requestToClient += listOfFiles.get(j)
+												.split("!")[0] + "!";
+									}
+								}
+								for (int j = 0; j < listFilesOnServer.size(); j++) {
+									// jika file pada client tidak terdapat pada
+									// server
+									if (!listOfFiles.contains(listFilesOnServer
+											.get(j))) {
+										// ini nanti dihapus
+										fileNotFoundOnServer
+												.add(listFilesOnServer.get(j)
+														.split("!")[0]);
+										// requestToClient +=
+										// listFilesOnServer.get(j).split("!")[0]+"!";
+									}
+								}
 
-			synchronized (this) {
-				for (int i = 0; i < listThreads.size(); i++) {
-					if (listThreads.get(i) != null) {
-						if (listThreads.get(i) == this) {
-//							sharedFolderPath = path;
-							break;
+								// SendFileToClient(fileNotFoundOnServer);
+								RequestFile(fileNotFoundOnClient);
+							}
 						}
 					}
-				}
-			}
-
-			while (true) {
-				int bytesRead;
-				int currentByte;
-
-				byte[] byteArrayLogFile = new byte[10000000];
-				fos = new FileOutputStream("log_"
-						+ clientSocket.getRemoteSocketAddress() + ".txt");
-				bos = new BufferedOutputStream(fos);
-				bytesRead = is.read(byteArrayLogFile, 0, byteArrayLogFile.length);
-				currentByte = bytesRead;
-				System.out.println("LOL");
-				do {
-					bytesRead = is.read(byteArrayLogFile, currentByte,
-							(byteArrayLogFile.length - currentByte));
-					if (bytesRead >= 0)
-						currentByte += bytesRead;
-				} while (bytesRead > -1);
-				System.out.println("LOL2");
-				bos.write(byteArrayLogFile);
-				bos.flush();
-				for (int i = 0; i < listThreads.size(); i++) {
-					if (listThreads.get(i) != null) {
-						// if (listThreads.get(i) != this) {
-						// listThreads.get(i).os.println("Username " + name
-						// + " send " + line);
-						// } else {
-						// listThreads.get(i).os.println(name + " : " + line);
-						// }
+				} else if (line.indexOf("quit") != -1) {
+					for (int i = 0; i < listThreads.size(); i++) {
+						if (listThreads.get(i) != null) {
+							if (listThreads.get(i) == this) {
+								os.println("quit");
+							}
+						}
 					}
+					System.out.println("Done quit");
+					break;
 				}
-				this.sleep(2500);
 			}
-//
-//			for (int i = 0; i < listThreads.size(); i++) {
-//				if (listThreads.get(i) != null && listThreads.get(i) != this) {
-//					// listThreads.get(i).os.println("Username " + name
-//					// + " is offline");
-//				}
-//			}
-//
-//			for (int i = 0; i < listThreads.size(); i++) {
-//				if (listThreads.get(i) == this) {
-//					listThreads.set(i, null);
-//				}
-//			}
 
-//			is.close();
-//			os.close();
-//			clientSocket.close();
+			for (int i = 0; i < listThreads.size(); i++) {
+				if (listThreads.get(i) == this) {
+					listThreads.set(i, null);
+				}
+			}
+
+			is.close();
+			os.close();
+			clientSocket.close();
+
 		} catch (IOException e) {
-			e.printStackTrace();
 			System.out.println("Oops, cannot established connection to : "
 					+ clientSocket.getRemoteSocketAddress());
-		} catch (InterruptedException e) {
 		}
-
 	}
 }
